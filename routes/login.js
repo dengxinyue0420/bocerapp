86f41a31b37d6a52a826fd9e5daa85b07b0af99c
dengxinyue0420 <dxy0420@mit.edu>
2015-09-06 17:19:51 -0400

    test

--- routes/login.js
+++ routes/login.js
@@ -8,7 +8,7 @@ router.get('/addUser', function(req, res, next){
 	res.send('receive add user request');
 });
 
-router.post('/addUser', function(req, res, next){
+router.post('/addUser', function(req, res){
 	console.log('receive add user request');
 	console.log(req);
 	res.write("{out:'success'}");
