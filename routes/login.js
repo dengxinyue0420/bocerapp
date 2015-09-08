d21a84f7df9854cd7fadbcce4ca197845f83ffa3
dengxinyue0420 <dxy0420@mit.edu>
2015-09-07 20:26:19 -0400

    check facebook

--- routes/login.js
+++ routes/login.js
@@ -30,21 +30,21 @@ router.post('/addUser', function(req, res){
 			}
 		}
 		else{
-			out.content='suceess';
+			out.content='success';
 		}
 		res.send(out);
 	});
 });
 
-router.post('/login', function(req, res, next){
+router.post('/login', function(req, res){
 	var username = req.body.username;
 	var password = req.body.username;
 
 	var out = {
 		'Target Action':'loginresult',
 		'content':''
-	}
-	User.find({username:username},{},function(err,users){
+	};
+	User.find({username:username},function(err,users){
 		if(err)
 			out.content='fail';
 		else if(users.length===0){
@@ -52,6 +52,45 @@ router.post('/login', function(req, res, next){
 		}else{
 			out.content='success';
 		}
+		res.send(out);
+	})
+});
+
+router.post('/checkFacebook', function(req,res){
+	var username = req.body.username;
+
+	var out = {
+		'Target Action':'facebookresult',
+		'content':''
+	};
+	User.find({username:username},function(err,data){
+		if(err){
+			out.content = 'fail';
+		}else if(data.length===0){
+			out.content = 'not exist';
+		}else{
+			out.content='success';
+		}
+		res.send(out);
+	});
+});
+
+router.post('forgetPassword', function(req,res){
+	var username = req.body.username;
+	var out = {
+		'Target Action':'forgetresult',
+		'content':''
+	}
+	User.find({username:username},function(err,data){
+		if(err){
+			out.content = 'fail';
+		}else if(data.length===0){
+			out.content = 'not exist';
+		}else{
+			out.content='success';
+			console.log('send email to user');
+		}
+		res.send(out);
 	})
 });
 
