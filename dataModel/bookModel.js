82bcda07ee94eb5a9d2cbc5165d99a8d1a4db2db
Xinyue Deng <dxy0420@mit.edu>
2015-10-04 22:06:03 -0400

    add book

---
+++ dataModel/bookModel.js
@@ -0,0 +1,34 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var crypto = require('crypto');
+
+
+var bookSchema = new Schema({
+	user_id:{type:Schema.ObjectId,ref:'User'},
+	title:{type:String, require:true},
+	ISBN:{type:String, require:true},
+	author:{type:String, require:true},
+	edition:{type:String},
+	image:[String],
+	className:{type:String},
+	price:{type:Number, require:true},
+	loc:{type:[Number], require:true},
+	state:{type:Number, default:0},
+	creatDate: Date,
+	is_deleted: Boolean},
+	{
+		collection:'books'
+	});
+
+bookSchema.pre('save',function(next){
+	var date = new Date();
+	this.creatDate = date;
+	this.is_deleted = false;
+
+	next();
+});
+
+
+var Book = mongoose.model('Book',bookSchema);
+
+module.exports=Book;
---
+++ routes/book.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+var Book = require('../dataModel/bookModel');
+var User = require('../dataModel/userModel');
+
+/* GET home page. */
+router.get('/addbook', function(req, res) {
+  	var image = [req.body.image1,req.body.image2,req.body.image3];
+  	var loc = [req.body.locationX,req.body.locationY];
+  	var user = User.findOne({username:req.body.username});
+
+  	var newBook = Book({
+  		user:user._id,
+  		title:req.body.title,
+  		author:req.body.author,
+		edition:req.body.edition,
+		ISBN:req.body.ISBN,
+		className:req.body.className,
+		price.req.body.price,
+		image:image,
+		loc:loc
+  	});
+  	newBook.save(function(err){
+  		var out = {
+		'Target Action':'addbookresult',
+		'content':''};
+		if(err){
+			out.content='fail';
+		}
+		else{
+			out.content='success';
+		}
+		res.send(out);
+	});
+});
+
+module.exports = router;
